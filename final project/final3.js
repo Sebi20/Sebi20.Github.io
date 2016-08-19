ebea9ce8f7d72148263f0c33e4340289ac7e26ba
Sebi20 <sebastep1@charter.newvisions.org>
2016-08-19 12:26:11 -0400

    new

---
+++ final project/final3.js
@@ -0,0 +1,10 @@
+
+
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
