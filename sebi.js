668410c77783403e2d50e89fed0da85306e7cb32
Sebi20 <sebastep1@charter.newvisions.org>
2016-08-18 20:54:35 -0400

    new

--- sebi.js
+++ sebi.js
@@ -34,11 +34,3 @@ function myFunction() {
 
 
 
-/*function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 */
-/*function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
