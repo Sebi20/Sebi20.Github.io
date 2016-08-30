9d44c42d2da5f975c72105e7eeaa0b05803d6c12
Sebi20 <sebastep1@charter.newvisions.org>
2016-08-30 19:53:04 -0400

    new

--- buckets/buckets.js
+++ buckets/buckets.js
@@ -4,18 +4,21 @@ var boundry1
 var boundry2
 var boundry3
 var boundry4
+
 function setup(){
-	createCanvas(800,800);
+	createCanvas(400,400);
+	textAlign(CENTER, CENTER);
 //framerate(7)
  paddle = createSprite(width/2,height-50,100,10);
  ball = createSprite(width/2, height/2, 10, 10);
   ball.maxSpeed = MAX_SPEED;
- ball.velocity.y=20
+ ball.velocity.y=10
  ball.velocity.x=20
-boundry1=createSprite(width/5,height-700,1000,20)	
-boundry2=createSprite(width/5,height-5,1500,20)
-boundry3=createSprite(width/5,height/200,1000,20)
-boundry4=createSprite(width/5,height/800,1000,20)
+boundry1=createSprite(width/1,height-400,1500,1);	
+boundry2=createSprite(width/5,height-1,1500,1);
+boundry3=createSprite(width-1, height/2, 1, 1000);
+boundry4=createSprite(1, height/2, 1, 1000);
+
 }
 
 function draw(){
@@ -31,7 +34,7 @@ if(ball.overlap(boundry1)){
 	} 
 if(ball.overlap(boundry2)){
 	ball.setVelocity(0,0);
-	//set.Text(GAME OVER);
+	
 	}
 if(ball.overlap(boundry3)){
 	ball.setVelocity(-ball.velocity.x,ball.velocity.y);
@@ -40,21 +43,17 @@ if(ball.overlap(boundry4)){
 	ball.setVelocity(-ball.velocity.x,ball.velocity.y);
 	}
 
-}
 
 
 
 
-//function ballMove(){
-
-//}
+ 
 
 
 
+ drawSprites();
 
-
-
-
+ }
 
 
 
@@ -68,24 +67,8 @@ if(ball.overlap(boundry4)){
 
 
 
-//function keyPressed() {
-//  if (keyCode == RIGHT_ARROW) {
- //   paddle .setSpeed(1.5, 0);
- // }
-//  else if (keyCode == DOWN_ARROW) {
- //   paddle .setSpeed(1.5, 90);
- // }
-  //else if (keyCode == LEFT_ARROW) {
-  //  paddle .setSpeed(1.5, 180);
- // }
- // else if (keyCode == UP_ARROW) {
- //   paddle .setSpeed(1.5, 270);
-  //}
- // else if (key == ' ') {
- //   paddle .setSpeed(4, 300);
- // }
- // return false;
-//}
+
+
 
 
 
