3f9e28dbcb54ce96d8b41d550d352d8cf54e4549
Sebastian <sebastian.stephenson1@gmail.com>
2022-04-16 12:40:11 -0400

    Added animation to the skills that make them spin on loading

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+let skill = document.querySelectorAll('.skill');
 
-
-
+for (let i in skill){
+    skill[i].style.animation = "rotateSkills 1s 3";
+    skill[i].animationTimingFunction = "ease-out"
+}
 
 
 
