41d30e2aaad968e4466792cc4042cad9ed6d8c19
Sebastian <sebastian.stephenson1@gmail.com>
2022-04-17 00:56:09 -0400

    Changed the rotateY to only rotate

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let skill = document.querySelectorAll('.skill');
 
 for (let i in skill){
-    skill[i].style.animation = "rotateSkills 1s 3";
+    skill[i].style.animation = "rotateSkills 1s 2";
     skill[i].animationTimingFunction = "ease-out"
 }
 
