a76f1ad8e3b5b2c0ef2b293e1e373365ab59d835
Sebastian <sebastian.stephenson1@gmail.com>
2022-04-17 13:05:04 -0400

    Paused the animation on the projects, wasn't happy with it

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let skill = document.querySelectorAll('.skill');
 const projects = document.querySelectorAll(".projects");
 
-for(let i = 0; i < projects.length; i++){
-    window.addEventListener('scroll', function(){
-        projects[i].style.animation = "spinProjects 2s 1"
-    })
-}
+// for(let i = 0; i < projects.length; i++){
+//     window.addEventListener('scroll', function(){
+//         projects[i].style.animation = "spinProjects 2s 1"
+//     })
+// }
 
 for (let i in skill){
     skill[i].style.animation = "rotateSkills 1s 2";
