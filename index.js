dc32ae90c226113045f9c28e95f54d562dc88a25
Sebastian <sebastian.stephenson1@gmail.com>
2022-08-18 17:40:39 -0400

    Added an email section to the page #closes #6

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const btn = document.getElementById('submitMessageBtn');
+
+document.getElementById('emailForm')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_w6sxi8d';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
+
+
 let skill = document.querySelectorAll('.skill');
 const projects = document.querySelectorAll(".projects");
 const headerImg = document.querySelectorAll('.headerImg');
@@ -12,10 +36,6 @@ for (let i in skill){
     skill[i].style.animation = "rotateSkills 1s 2";
     skill[i].animationTimingFunction = "ease-out"
 }
-
-
-
-
 
 
 
